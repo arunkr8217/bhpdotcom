815e91c5d0ba8104c46393206d860a7b63c866e9
mohit malhotra <vaishaliramgarhia2@gmail.com>
2019-05-29 10:52:05 +0530

    add user roledetail model

--- bhptale/models.py
+++ bhptale/models.py
@@ -6,6 +6,18 @@ class UserRole(models.Model):
     role=models.CharField(max_length=225,unique=True,default="")
     def __str__(self):
         return self.role
+class RoleDetails(models.Model):
+    role=models.ForeignKey(UserRole,on_delete=models.CASCADE)
+    email=models.CharField(primary_key=True, max_length=255, default="")
+    username=models.CharField(unique=True,null=True,max_length=255, default="")
+    fullname=models.CharField(max_length=225,null=True, default="")
+    mobile=models.BigIntegerField(unique=True, default="")
+    address=models.CharField(max_length=225,null=True, default="")
+    gender=models.TextField(max_length=10,null=True, default="")
+    otp=models.CharField(max_length=255,null=True, default="")
+    otp_time=models.CharField(max_length=255,null=True, default="")
+    verify_link=models.CharField(max_length=255,null=True, default="")
+    active=models.NullBooleanField(default="")
 
 
 #after this run these commands
