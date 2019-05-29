3d79131c727e2cf1a49706a662d964f2532b8aa4
mohit malhotra <vaishaliramgarhia2@gmail.com>
2019-05-29 10:58:02 +0530

    add model in admin panel

--- bhptale/admin.py
+++ bhptale/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from bhptale.models import UserRole
+from bhptale.models import UserRole,RoleDetails
+
 # Register your models here.
 admin.site.register(UserRole)
+
+admin.site.register(RoleDetails)
