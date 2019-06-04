794314373a28b8791b032aeeece2b872b113d0a3
ARUN SHARMA <arunkr8217@gmail.com>
2019-06-04 11:46:57 +0530

    models file (passowrd added)

--- bhptale/models.py
+++ bhptale/models.py
@@ -3,20 +3,44 @@ from django.db import models
 # Create your models here
 class UserRole(models.Model):
     id=models.AutoField(primary_key=True)
-    role=models.CharField(max_length=225,unique=True,default="")
+    role=models.CharField(max_length=225,
+                          unique=True,
+                          default="")
     def __str__(self):
         return self.role
 class RoleDetails(models.Model):
-    role=models.ForeignKey(UserRole,on_delete=models.CASCADE)
-    email=models.CharField(primary_key=True, max_length=255, default="")
-    username=models.CharField(unique=True,null=True,max_length=255, default="")
-    fullname=models.CharField(max_length=225,null=True, default="")
-    mobile=models.BigIntegerField(unique=True, default="")
-    address=models.CharField(max_length=225,null=True, default="")
-    gender=models.TextField(max_length=10,null=True, default="")
-    otp=models.CharField(max_length=255,null=True, default="")
-    otp_time=models.CharField(max_length=255,null=True, default="")
-    verify_link=models.CharField(max_length=255,null=True, default="")
+    role=models.ForeignKey(UserRole,
+                           on_delete=models.CASCADE)
+    email=models.CharField(primary_key=True,
+                           max_length=255,
+                           default="")
+    password=models.CharField(max_length=225,
+                              default="")
+
+    username=models.CharField(unique=True,
+                              null=True,
+                              max_length=255,
+                              default="")
+    fullname=models.CharField(max_length=225,
+                              null=True,
+                              default="")
+    mobile=models.BigIntegerField(unique=True,
+                                  default="")
+    address=models.CharField(max_length=225,
+                             null=True,
+                             default="")
+    gender=models.CharField(max_length=10,
+                            null=True,
+                            default="")
+    otp=models.CharField(max_length=255,
+                         null=True,
+                         default="")
+    otp_time=models.CharField(max_length=255,
+                              null=True,
+                              default="")
+    verify_link=models.CharField(max_length=255,
+                                 null=True,
+                                 default="")
     active=models.NullBooleanField(default="")
 
 
